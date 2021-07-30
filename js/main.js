7bb20ec435e001c96abaaa177c83e75af0715c64
Olena Honcharuk <gasmin07@gmail.com>
2021-07-30 21:48:27 +0300

    done: media

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ var swiper = new Swiper(".mySwiper", {
   freeMode: true,
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
+  breakpoints: {
+    320: {
+      spaceBetween: 10,
+      slidesPerView: 2,
+    },
+    480: {
+      spaceBetween: 20,
+      slidesPerView: 3,
+    },
+    640: {
+      spaceBetween: 40,
+      slidesPerView: 4,
+    },
+  },
 });
 var swiper2 = new Swiper(".mySwiper2", {
   spaceBetween: 10,
@@ -11,4 +25,13 @@ var swiper2 = new Swiper(".mySwiper2", {
   thumbs: {
     swiper: swiper,
   },
+});
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuCloseBtn = document.querySelector(".close-menu");
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
+menuCloseBtn.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
 });
